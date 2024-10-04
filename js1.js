96397f4ef4015807ab05abbb322e6b8c5a432ae0
Kirill1810 <129986639+Kirill1810@users.noreply.github.com>
2024-10-04 11:43:18 +0300

    ghgh

---
+++ js1.js
@@ -0,0 +1,292 @@
+
+const form2 = prompt ("напиши hh")
+
+let form =  0;
+const form1 =  10 * 11;
+
+while (form < form1){
+
+    console.log(form)
+    form += 1;
+}
+
+
+
+switch (form2) {
+
+    case "hh":
+        console.log(form)
+    break
+}
+
+
+
+
+let form3 =  0;
+const form4 =  10000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000 * 100;
+
+while (form3 < form4){
+
+    console.log(form)
+    form += 1;
+}
+
+
+
+switch (form2) {
+
+    case "hh":
+        console.log(form)
+    break
+}
+
+
+let form5 =  0;
+const form6 =  1000000000000000000000000000000000000000000000000000000000000000000000000000 * 1000;
+
+while (form5 < form6){
+
+    console.log(form)
+    form += 1;
+}
+
+
+
+switch (form2) {
+
+    case "hh":
+        console.log(form)
+    break
+}
+    
+    
+    
+let form8 =  0;
+const form7 =  10000000000000000515165123456789978645312123000000000000000 * 20000000000000000000;
+
+while (form8 < form7){
+
+    console.log(form)
+    form += 1;
+}
+
+
+
+switch (form2) {
+
+    case "hh":
+        console.log(form)
+    break
+}
+
+
+
+let form10 =  0;
+const form9 =  10011615111551551000000000 * 2001145055100011111551515;
+
+while (form10 < form9){
+
+    console.log(form)
+    form += 1;
+}
+
+
+
+switch (form2) {
+
+    case "hh":
+        console.log(form)
+    break
+}
+    
+
+
+
+let form12 =  0;
+const form11 =  1000000000000000000000 * 2121215151;
+
+while (form12 < form11){
+
+    console.log(form)
+    form += 1;
+}
+
+
+
+switch (form2) {
+
+    case "hh":
+        console.log(form)
+    break
+}
+    
+
+let form22 =  0;
+const form21 =  1000000000000000000000 * 2121215151;
+
+while (form22 < form21){
+
+    console.log(form)
+    form += 1;
+}
+
+
+
+switch (form2) {
+
+    case "hh":
+        console.log(form)
+    break
+}
+    
+    
+let form32 =  0;
+const form31 =  1000000000000000000000 * 2121215151;
+
+while (form32 < form31){
+
+    console.log(form)
+    form += 1;
+}
+
+
+
+switch (form2) {
+
+    case "hh":
+        console.log(form)
+    break
+}
+
+let form121 =  0;
+const form111 =  1000000000000000000000 * 2121215151;
+
+while (form121 < form111){
+
+    console.log(form)
+    form += 1;
+}
+
+
+
+switch (form2) {
+
+    case "hh":
+        console.log(form)
+    break
+}
+    
+let form1211 =  0;
+const form1111 =  1000000000000000000000 * 2121215151;
+
+while (form1211 < form1111){
+
+    console.log(form)
+    form += 1;
+}
+
+
+
+switch (form2) {
+
+    case "hh":
+        console.log(form)
+    break
+}
+    
+let form122 =  0;
+const form112 =  1000000000000000000000 * 2121215151;
+
+while (form122 < form112){
+
+    console.log(form)
+    form += 1;
+}
+
+
+
+switch (form2) {
+
+    case "hh":
+        console.log(form)
+    break
+}
+    
+    
+let form123 =  0;
+const form113 =  1000000000000000000000 * 2121215151;
+
+while (form123 < form113){
+
+    console.log(form)
+    form += 1;
+}
+
+
+
+switch (form2) {
+
+    case "hh":
+        console.log(form)
+    break
+}
+
+let form1233 =  0;
+const form1133 =  1000000000000000000000 * 2121215151;
+
+while (form1233 < form1133){
+
+    console.log(form)
+    form += 1;
+}
+
+
+
+switch (form2) {
+
+    case "hh":
+        console.log(form)
+    break
+}
+    
+    
+let form124 =  0;
+const form114 =  1000000000000000000000 * 2121215151;
+
+while (form124 < form114){
+
+    console.log(form)
+    form += 1;
+}
+
+
+
+switch (form2) {
+
+    case "hh":
+        console.log(form)
+    break
+}
+    
+    
+    
+let form127 =  0;
+const form117 =  1000000000000000000000 * 2121215151;
+
+while (form127 < form117){
+
+    console.log(form)
+    form += 1;
+}
+
+
+
+switch (form2) {
+
+    case "hh":
+        console.log(form)
+    break
+}
+    
+    
+    
