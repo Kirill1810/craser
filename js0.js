fa7581bd720b0e8719e6e669848f461b4e98627c
Kirill1810 <129986639+Kirill1810@users.noreply.github.com>
2025-01-30 21:48:25 +0200

    gh

---
+++ js0.js
@@ -0,0 +1,30 @@
+const paragraphs = document.querySelectorAll('div');
+
+
+paragraphs.forEach(function(div) {
+    div.classList.add('d1');
+  });
+
+
+  const paragaphs = document.querySelectorAll('div');
+
+
+paragaphs.forEach(function(div) {
+    div.classList.add('d2');
+  });
+
+  const paragahs = document.querySelectorAll('div');
+
+  paragaphs.forEach(function(div) {
+    div.classList.add('d3');
+
+  
+  })
+  
+  
+
+
+
+
+
+ 
